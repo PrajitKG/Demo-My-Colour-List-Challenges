f39367fa21d4505377fa5fd4b2d75d549712451c
PrajitKG <132332466+PrajitKG@users.noreply.github.com>
2023-06-03 14:48:43 +0800

    Initial commit

---
+++ app/src/main/java/com/example/demomycolourlistchallenges/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.demomycolourlistchallenges;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText etElement;
+    Button btnAdd;
+    ListView lvColour;
+    EditText etIndexElement;
+    Button btnRemove;
+    Button btnUpdate;
+
+    ArrayList<String> alColours = new ArrayList<String>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etElement = findViewById(R.id.editTextColour);
+        btnAdd = findViewById(R.id.buttonAddItem);
+        lvColour = findViewById(R.id.listViewColour);
+        etIndexElement = findViewById(R.id.editTextIndex);
+        btnRemove = findViewById(R.id.buttonRemoveItem);
+        btnUpdate = findViewById(R.id.buttonUpdateItem);
+        alColours.add("Red");
+        alColours.add("Orange");
+        ArrayAdapter aaColour = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,alColours);
+        lvColour.setAdapter(aaColour);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String colour = etElement.getText().toString();
+                int pos = Integer.parseInt(etIndexElement.getText().toString());
+                alColours.add(pos,colour);
+                aaColour.notifyDataSetChanged();
+
+
+            }
+        });
+        lvColour.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String colour = alColours.get(position);
+                Toast.makeText(getApplicationContext(), colour, Toast.LENGTH_SHORT).show();;
+            }
+        });
+        btnRemove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int pos = Integer.parseInt(etIndexElement.getText().toString());
+                alColours.remove(pos);
+                aaColour.notifyDataSetChanged();
+            }
+        });
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String colour = etElement.getText().toString();
+                int pos = Integer.parseInt(etIndexElement.getText().toString());
+                alColours.set(pos,colour);
+                aaColour.notifyDataSetChanged();
+            }
+        });
+
+    }
+}
